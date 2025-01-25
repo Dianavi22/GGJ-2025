850768460946e5296fc8abfc9ea23b4aa27ee9e9
KevinF <kvnfagot@gmail.com>
2025-01-25 18:31:10 +0100

    change game scene for menuiing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,23 @@
 using System;
 using UnityEngine;
 
-namespace Assets.Scripts
-{
-    internal class GameManager
-    {
-        public bool isPlaying  = true;
+namespace Assets.Scripts {
+    internal class GameManager : MonoBehaviour{
+
+        [Header("Public Attributes")]
+        public bool isPlaying = false;
+
+        [Header("Game Canvas")]
+        [SerializeField] Canvas MainMenuCanvas;
+        [SerializeField] Canvas PauseMenuCanvas;
+        [SerializeField] Canvas GameOverCanvas;
+        [SerializeField] Canvas UICanvas;
+
+        [Header("End Game Text")]
+        [SerializeField] GameObject WinText;
+        [SerializeField] GameObject DeadText;
+
+
         private int _level = 0;
         private int _score = 0;
 
@@ -17,27 +29,22 @@ namespace Assets.Scripts
 
         private static GameManager _instance;
 
-        public static GameManager Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
+        private bool _isPaused = false;
+
+        public static GameManager Instance {
+            get {
+                if (_instance == null) {
                     _instance = new GameManager();
                 }
                 return _instance;
             }
         }
 
-        private GameManager() { }
-
-        public int GetLevel()
-        {
+        public int GetLevel() {
             return _level;
         }
 
-        public void Score()
-        {
+        public void Score() {
             _score += _score;
 
             // This means that every 10 times that we score, we increase the level.
@@ -48,59 +55,84 @@ namespace Assets.Scripts
         /// Used for UI
         /// </summary>
         /// <returns></returns>
-        public float GetPercentage()
-        {
+        public float GetPercentage() {
             // ex: 30,30921 x 100 => rounded = 3031 => rounded / 100 => 30,31% 
             return MathF.Round(_precision_percentage * 100) / 100;
         }
 
-        public void ShootMissed()
-        {
+        public void ShootMissed() {
             _nb_shoots++;
             _precision_percentage = _nb_sucessful_shoots / _nb_shoots;
             UpdateWeaponLevel();
         }
 
-        public void ShootSucessful()
-        {
+        public void ShootSucessful() {
             _nb_shoots++;
             _nb_sucessful_shoots++;
             _precision_percentage = _nb_sucessful_shoots / _nb_shoots;
             UpdateWeaponLevel();
         }
 
-        public void UpdateWeaponLevel()
-        {
-            if (_precision_percentage > 99)
-            {
+        public void UpdateWeaponLevel() {
+            if (_precision_percentage > 99) {
                 _weaponLevelEnum = WeaponTypeEnum.Level4;
-            }
-            else if (_precision_percentage > 90)
-            {
+            } else if (_precision_percentage > 90) {
                 _weaponLevelEnum = WeaponTypeEnum.Level3;
-            }
-            else if (_precision_percentage > 88)
-            {
+            } else if (_precision_percentage > 88) {
                 _weaponLevelEnum = WeaponTypeEnum.Level2;
-            }
-            else
-            {
+            } else {
                 _weaponLevelEnum = WeaponTypeEnum.Level1;
             }
         }
 
+
+        public WeaponTypeEnum GetWeaponLevelEnum() {
+            return _weaponLevelEnum;
+        }
+
+        // Game Managing Canvas Display
+
+        public void StartGame() {
+            isPlaying = true;
+            UICanvas.gameObject.SetActive(isPlaying);
+        }
         public void GameOver() {
             isPlaying = false;
+            GameOverCanvas.gameObject.SetActive(true);
+            WinText.SetActive(false);
+            DeadText.SetActive(true);
         }
 
         public void Win() {
             Debug.Log("Win");
             isPlaying = false;
+            WinText.SetActive(true);
+            DeadText.SetActive(false);
         }
 
-        public WeaponTypeEnum GetWeaponLevelEnum()
-        {
-            return _weaponLevelEnum;
+        public void QuitGame() {
+            Application.Quit();
         }
+
+        public void BackToMainMen() {
+            isPlaying = false;
+            MainMenuCanvas.gameObject.SetActive(true);
+            ResetGame();
+        }
+
+        public void Pause() {
+            if (Input.GetKeyDown(KeyCode.P) && isPlaying) {
+                _isPaused = !_isPaused;
+                if (_isPaused) {
+                    Time.timeScale = 0f;
+                    PauseMenuCanvas.gameObject.SetActive(true);
+                } else {
+                    PauseMenuCanvas.gameObject.SetActive(false);
+                    Time.timeScale = 1f;
+                }
+            }
+        }
+
+        public void ResetGame() { }
     }
 }
